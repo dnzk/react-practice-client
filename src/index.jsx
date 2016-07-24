a65c144c737ee4c6d05174527a7e7781c8159859
dnzk <danishkalesaran@gmail.com>
2016-07-24 12:38:05 +0700

    separte voting into sub components

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,6 @@ import Voting from './components/Voting';
 const pair = ['Trainspotting', '28 days later'];
 
 ReactDOM.render(
-  <Voting pair={pair} />,
+  <Voting pair={pair} hasVoted="Trainspotting" />,
   document.getElementById('app')
 );
